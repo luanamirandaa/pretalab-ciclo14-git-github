4a2f5b0e79a83797aebded01942ee35de464bd0b
luanamirandaa <luanamiranda.contato@gmail.com>
2025-09-20 13:45:50 +0100

    primeiro comit: aprendizado git-pretalab

---
+++ git-github.py
@@ -0,0 +1,6 @@
+"""
+Arquivo: aprendizado_git_github.py
+Descrição: Este arquivo é parte do meu estudo e prática sobre Git e GitHub.
+Objetivo: Aprender e aplicar conceitos de versionamento de código, criação de repositórios,
+commits e boas práticas de colaboração usando Git e GitHub.
+Autor: Luana Miranda
